2ad7f866ee5bcdf8ebdd9d6f93e3352e4cd58c15
TheAsiyaS <theasiya.s@gmail.com>
2026-07-01 07:02:11 +0530

    if else

---
+++ controlstatement.py
@@ -0,0 +1,12 @@
+print('Hi,  number check')
+print('Enter a number :')
+''' num = int(input())
+# if else 
+if num%2 == 0:
+    print("even")
+else : 
+    print("odd") '''
+
+#else if (nested)
+
+print
