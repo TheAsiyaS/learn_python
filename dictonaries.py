ccf2b688ff3b657beedb2d6b6ba04865dae00c8c
TheAsiyaS <theasiya.s@gmail.com>
2026-07-14 17:00:12 +0530

    feat : modules calling - normal way

--- dictonaries.py
+++ dictonaries.py
@@ -1,10 +1,11 @@
 #like map
-
-userdata = {
+def dicPract():
+  userdata = {
     'name' : "Asiya",
     'Age'  : "20",
     'place': "India"
 
-}
-userdata['Age'] = '19'#Change the value
-print(userdata['Age'])
+ }
+  userdata['Age'] = '19'#Change the value
+  print(userdata['Age'])
+  
